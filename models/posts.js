351a1cf22f56ce938afab1d20491f845d09f0c10
lotsofluck <anitabangali@yahoo.com>
2021-06-22 21:45:40 -0500

    Post tests implemented

---
+++ models/posts.js
@@ -0,0 +1,40 @@
+const Post = require('../models/post');
+
+module.exports = (app) => {
+
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
+    post.save(() => res.redirect('/'));
+  });
+
+};
+
+app.get('/', (req, res) => {
+  Post.find({}).lean()
+    .then((posts) => res.render('posts-index', { posts }))
+    .catch((err) => {
+      console.log(err.message);
+    })
+});
+
+app.get('/posts/:id', (req, res) => {
+  Post.findById(req.params.id).lean()
+    .then((post) => res.render('posts-show', { post }))
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+it('Should create with valid attributes at POST /posts/new', function(done) {
+
+...
+
+});
+
+after(function () {
+  Post.findOneAndDelete(newPost);
+});
---
+++ server.js
@@ -0,0 +1,8 @@
+require('./controllers/posts')(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set db
+require('./data/reddit-db');
+module.exports = app;
