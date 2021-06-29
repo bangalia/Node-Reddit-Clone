5731e704386f36046ec30fa2063e9a06ade7bedb
lotsofluck <anitabangali@yahoo.com>
2021-06-29 00:22:33 -0500

    Users can sign up

--- models/posts.js
+++ models/posts.js
@@ -1,70 +1,14 @@
-const Post = require('../models/post');
-const Comment = require('../models/comment');
+const { Schema, model } = require('mongoose');
 
-module.exports = (app) => {
+const postSchema = new Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  summary: { type: String, required: true },
+  subreddit: {type: String, required: true},
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  upVotes : [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  downVotes : [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  voteScore : { type: Number },
+}, { timestamps: true });
 
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
-
-    // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
-    post.save(() => res.redirect('/'));
-  });
-
-};
-
-app.get('/', (req, res) => {
-  Post.find({}).lean()
-    .then((posts) => res.render('posts-index', { posts }))
-    .catch((err) => {
-      console.log(err.message);
-    })
-});
-
-app.get('/posts/:id', (req, res) => {
-  Post.findById(req.params.id).lean()
-    .then((post) => res.render('posts-show', { post }))
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-app.get('/n/:subreddit', (req, res) => {
-  Post.find({ subreddit: req.params.subreddit }).lean()
-    .then((posts) => res.render('posts-index', { posts }))
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-it('Should create with valid attributes at POST /posts/new', function(done) {
-
-...
-
-});
-
-app.post('/posts/:postId/comments', (req, res) => {
-  // INSTANTIATE INSTANCE OF MODEL
-  const comment = new Comment(req.body);
-
-  // SAVE INSTANCE OF Comment MODEL TO DB
-  comment
-    .save()
-    .then(() => Post.findById(req.params.postId))
-    .then((post) => {
-      post.comments.unshift(comment);
-      return post.save();
-    })
-    .then(() => res.redirect('/'))
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-subreddit: { type: String, required: true },
-comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-
-after(function () {
-  Post.findOneAndDelete(newPost);
-});
+module.exports = model('Post', postSchema);
