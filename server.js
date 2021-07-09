78155a44aa7b40fe6e7a04ac39613ee4dcdff244
lotsofluck <anitabangali@yahoo.com>
2021-07-09 10:07:40 -0500

    User can comment

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 // Require Libraries
+const dotenv = require('dotenv').config();
 const express = require('express');
 const https = require('https');
 const exphbs  = require('express-handlebars');
+const app = express()
 const port = 3000
 
 // App Setup
 const app = express();
-app.use(express.static('public'));
+
 
 // Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static('public'));
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// DATABASE
+require('./data/reddit-db');
+
+
+const checkAuth = require('./middleware/checkAuth');
+
+
+require('./routes/posts')(app);
+require('./routes/comments.js')(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+module.exports = app;
